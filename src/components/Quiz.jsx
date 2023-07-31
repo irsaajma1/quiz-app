45a2325676a5a2cb688e43c03e0010d91a74dcfe
irsaajma1 <irsaajmal1234@gmail.com>
2023-07-31 14:55:40 -0700

    complete quiz task in react

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,70 @@
+import React,{useState} from "react";
+import  QuizData  from "./QuizData";
+import QuizResult from"./QuizResult";
+import './Quiz.css';
+
+const Quiz=()=>{
+     
+    const [currentQuestion,setCurrentQuestion]= useState(0);
+    const [score,setScore] = useState(0);
+    const[takeanswer,setTakeAnswer]= useState(0);
+    const [showResult,setShowResult]=useState(false);
+   
+    const onClickHandler=()=>{
+       updateQuestion();
+        if(currentQuestion< QuizData.length-1){
+        setCurrentQuestion(currentQuestion+1);
+        setTakeAnswer(0);
+        }
+        else{
+           setShowResult(true);
+        }
+
+    }
+
+    const updateQuestion=()=>{
+     
+        console.log(QuizData[currentQuestion].answer);
+        if(takeanswer === QuizData[currentQuestion].answer)
+        {
+           setScore(score+1);
+         
+        }
+    }
+   
+    const resetAll=()=>
+    {
+        setCurrentQuestion(0);
+        setScore(0);
+        setTakeAnswer(0);
+        setShowResult(false);
+    }
+    return(
+<div>
+   
+    <div className="container">
+    {showResult ? ( <QuizResult score={score} totalScore={QuizData.length} tryAgain={resetAll}/>):(
+                <>
+        <p className="heading-text">Quiz APP</p>
+        <div className="question">
+        <span id="question-number">{currentQuestion+1}</span>
+        <span id="question-txt">{QuizData[currentQuestion].question}</span>
+        </div>
+        <div className="option-container">
+            {QuizData[currentQuestion].option.map((options,i)=>{
+               return(
+               <buttons className= {`option-btn ${takeanswer == i+1?"checked":null}`}key={i} onClick={()=>{setTakeAnswer(i+1)}}>
+                    {options};
+                </buttons>
+            )})}
+        </div>
+        <input type="button" value="Next" id="next-button" onClick={onClickHandler}></input>
+        </>)}
+    </div>
+
+</div>
+
+    )
+
+}
+export default Quiz;
---
+++ src/components/QuizResult.jsx
@@ -0,0 +1,18 @@
+import './Quiz.css';
+const QuizResult=(props)=>{
+    return(
+        <div>
+  <div className="container">
+  
+    <div className="show-score">
+      You Scored:{props.score}<br></br>
+       Out of :{props.totalScore}
+    </div>
+    
+    <button type="button" value="Next" id="next-button" onClick={props.tryAgain}> TRY AGAIN</button>
+      
+  </div>
+  </div>
+    );
+}
+export default QuizResult;
